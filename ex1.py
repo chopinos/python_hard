93a150df43ae66b33854b0fa69f9bef667527dcc
lol <user02@work>
2015-01-12 12:30:49 +0100

    ex1

---
+++ ex1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+
+print 'Dupa'
+print "'Hello' there"
+#print '"Hello :" '"'"
